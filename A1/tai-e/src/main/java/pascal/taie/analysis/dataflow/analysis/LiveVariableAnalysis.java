f6b29cd0c2f5d4d15c402fd4b5b6f3954eb0f3b3
cardigan1008 <211250058@smail.nju.edu.cn>
2024-05-07 20:20:21 +0800

    feat: Update LiveVariableAnalysis

--- A1/tai-e/src/main/java/pascal/taie/analysis/dataflow/analysis/LiveVariableAnalysis.java
+++ A1/tai-e/src/main/java/pascal/taie/analysis/dataflow/analysis/LiveVariableAnalysis.java
@@ -25,6 +25,8 @@ package pascal.taie.analysis.dataflow.analysis;
 import pascal.taie.analysis.dataflow.fact.SetFact;
 import pascal.taie.analysis.graph.cfg.CFG;
 import pascal.taie.config.AnalysisConfig;
+import pascal.taie.ir.exp.LValue;
+import pascal.taie.ir.exp.RValue;
 import pascal.taie.ir.exp.Var;
 import pascal.taie.ir.stmt.Stmt;
 
@@ -45,26 +47,49 @@ public class LiveVariableAnalysis extends
         return false;
     }
 
+    // For liveness analysis, the initial boundary fact is an empty set.
     @Override
     public SetFact<Var> newBoundaryFact(CFG<Stmt> cfg) {
-        // TODO - finish me
-        return null;
+        return new SetFact<Var>();
     }
 
+    // For liveness analysis, the initial fact for each bb is an empty set.
     @Override
     public SetFact<Var> newInitialFact() {
-        // TODO - finish me
-        return null;
+        return new SetFact<Var>();
     }
 
+    // For liveness analysis, moveinto is to union fact(IN) into target(OUT).
     @Override
     public void meetInto(SetFact<Var> fact, SetFact<Var> target) {
-        // TODO - finish me
+        target.union(fact);
     }
 
+    // For liveness analysis, transfer function here is to calculate in = use U (out - def).
     @Override
     public boolean transferNode(Stmt stmt, SetFact<Var> in, SetFact<Var> out) {
-        // TODO - finish me
+        // Check whether the stmt is var = x, otherwise, no function will be applied.
+        if (stmt.getDef().isPresent()) {
+            LValue def = stmt.getDef().get();
+
+            // We'll only consider vars on the left hand.
+            if (def instanceof Var) {
+                // Calculate uses (vars on the right hand) into a set.
+                SetFact<Var> use = new SetFact<>();
+                for (RValue rVar: stmt.getUses()) {
+                    if (rVar instanceof Var) {
+                        use.add((Var) rVar);
+                    }
+                }
+                // in = use U (out - def).
+                out.remove((Var) def);
+                use.union(out);
+                in = use;
+
+                return true;
+            }
+            return false;
+        }
         return false;
     }
 }
